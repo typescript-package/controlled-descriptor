d1968d0167d4bbd73cb0c27abc3e379096fe7299
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2025-08-17 16:45:16 +0000

    refactor(`ControlledDescriptor`): add the default controller.

--- src/lib/controlled-descriptor.class.ts
+++ src/lib/controlled-descriptor.class.ts
@@ -1,21 +1,23 @@
 // Abstract.
 import { ControlledDescriptorBase } from './controlled-descriptor-base.abstract';
+// Class.
+import { ControlledDescriptorController } from '@typescript-package/controller';
 // Interface.
 import { ControlledPropertyDescriptor } from '@typedly/controlled-descriptor';
 import { WrappedPropertyDescriptor } from '@typedly/descriptor';
 import { WrappedPropertyDescriptorController } from '@typedly/controller';
 /**
- * @description
+ * @description The concrete implementation controlled descriptor class with controller to control its behavior.
  * @export
  * @class ControlledDescriptor
- * @template [O=any] 
- * @template {keyof O} [K=keyof O] 
- * @template {K extends keyof O ? O[K] : any} [V=K extends keyof O ? O[K] : any] 
- * @template {boolean} [A=boolean] 
- * @template {boolean} [N=boolean] 
- * @template {boolean} [C=boolean] 
- * @template {boolean} [E=boolean] 
- * @template {ControlledPropertyDescriptor<O, K, V, A, N, C, E, D>} [D=ControlledPropertyDescriptor<O, K, V, A, N, C, E, any>] 
+ * @template [O=any] The type of object.
+ * @template {keyof O} [K=keyof O] The type of the key.
+ * @template {K extends keyof O ? O[K] : any} [V=K extends keyof O ? O[K] : any] The type of the value.
+ * @template {boolean} [A=boolean] The type of the active.
+ * @template {boolean} [N=boolean] The type of the enabled.
+ * @template {boolean} [C=boolean] The type of the configurable.
+ * @template {boolean} [E=boolean] The type of the enumerable.
+ * @template {ControlledPropertyDescriptor<O, K, V, A, N, C, E, D>} [D=ControlledPropertyDescriptor<O, K, V, A, N, C, E, any>] The type of previous and `get`, `set` descriptor.
  * @extends {ControlledDescriptorBase<O, K, V, A, N, C, E, D>}
  */
 export class ControlledDescriptor<
@@ -43,20 +45,20 @@ export class ControlledDescriptor<
    * @type {string}
    */
   public get [Symbol.toStringTag](): string {
-    return 'ControllableDescriptor';
+    return 'ControlledDescriptor';
   }
 
   /**
    * Creates an instance of `ControlledDescriptor`.
    * @constructor
-   * @param {O} object 
-   * @param {K} key 
-   * @param {Partial<WrappedPropertyDescriptor<O, K, V, A, N, C, E, D>>} [attributes={}] 
+   * @param {O} object The object of the key.
+   * @param {K} key The object key.
+   * @param {Partial<WrappedPropertyDescriptor<O, K, V, A, N, C, E, D>>} [attributes={}] The attributes of the descriptor.
    * @param {new (
    *       object: O,
    *       key: K,
    *       descriptor: Partial<WrappedPropertyDescriptor<O, K, V, A, N, C, E, D>>,
-   *     ) => WrappedPropertyDescriptorController<O, K, V, A, N, C, E, D>} controller 
+   *     ) => WrappedPropertyDescriptorController<O, K, V, A, N, C, E, D>} [controller=ControlledDescriptorController] The controller to control accessors.
    */
   constructor(
     object: O,
@@ -66,7 +68,7 @@ export class ControlledDescriptor<
       object: O,
       key: K,
       descriptor: Partial<WrappedPropertyDescriptor<O, K, V, A, N, C, E, D>>,
-    ) => WrappedPropertyDescriptorController<O, K, V, A, N, C, E, D>
+    ) => WrappedPropertyDescriptorController<O, K, V, A, N, C, E, D> = ControlledDescriptorController
   ) {
     super(object, key, attributes, controller);
   }
